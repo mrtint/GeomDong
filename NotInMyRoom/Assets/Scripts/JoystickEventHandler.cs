b24404158ebd6439e938185652ea903cc998a7ff
Mr. Tint <mrtint0729@gmail.com>
2014-06-22 00:16:43 +0900

    f

---
+++ NotInMyRoom/Assets/Scripts/JoystickEventHandler.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JoystickEventHandler : MonoBehaviour {
+	[SerializeField]
+	private Animator animator;
+	[SerializeField]
+	private ThirdPersonCamera gamecam;
+
+	void Start(){
+
+	}
+
+	void OnEnable(){
+		EasyJoystick.On_JoystickMove += On_JoystickMove;
+		EasyJoystick.On_JoystickMoveEnd += On_JoystickMoveEnd;
+	}
+	
+	void OnDisable(){
+		EasyJoystick.On_JoystickMove -= On_JoystickMove	;
+		EasyJoystick.On_JoystickMoveEnd -= On_JoystickMoveEnd;
+	}
+	
+	void OnDestroy(){
+		EasyJoystick.On_JoystickMove -= On_JoystickMove;	
+		EasyJoystick.On_JoystickMoveEnd -= On_JoystickMoveEnd;
+	}
+
+	void On_JoystickMoveEnd(MovingJoystick move){
+		Debug.Log("MoveEnd");
+	}
+
+	void On_JoystickMove(MovingJoystick move){
+		Debug.Log("MoveStart");
+		float y = move.joystickAxis.y;
+		float x = move.joystickAxis.x;
+		Debug.Log ("x = " + x + " / " + "y = " + y);
+	}
+}
